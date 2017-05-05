34511ad04d24f43f2175fda3ae1967c9cbb97e0f
Meoso <meoso@users.noreply.github.com>
2017-05-05 08:40:44 -0500

    new file:   .gitignore 	renamed:    config.py -> config.py.sample 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -4,30 +4,35 @@
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root."
-  exit
+  exit 1
 fi
 
-IFACE=`grep IFACE config.py | cut -d'=' -f 2 | sed "s/['\" ]//g"`
+
+if [ ! -f config.py ]; then
+    echo 'Copying config.py.sample to config.py'
+    #copy with user, not root
+    sudo -u "$SUDO_USER" cp "config.py.sample" "config.py"
+fi
+
+IFACE=$(grep IFACE config.py | cut -d'=' -f 2 | sed "s/['\" ]//g")
 
 if [ "$IFACE" == "" ]; then
-	IFACE=`iwconfig 2>&1 | grep IEEE | cut -d" " -f 1`;
+	IFACE=$(iwconfig 2>&1 | grep IEEE | cut -d" " -f 1)
 fi
 
-if ( ifconfig "$IFACE" down ) ; then
+if ( ifconfig "$IFACE" down >/dev/null 2>&1 ) ; then
 	echo "$IFACE downed."
 fi
 
-#iw dev $IFACE interface add $IFACE type monitor
-
-if ( iwconfig "$IFACE" mode mon ) ; then
+if ( iwconfig "$IFACE" mode mon) ; then
 	echo "$IFACE entered Monitor-Mode"
-	if ( ifconfig "$IFACE" up ) ; then
+	if ( ifconfig "$IFACE" up >/dev/null 2>&1) ; then
 		echo "$IFACE upped  ; running core.py ; Ctrl-C once to stop."
 		python ./core.py
+	else
+		echo "Could not up $IFACE"
 	fi
 fi
-if ( ifconfig "$IFACE" down ) ; then
+if ( ifconfig "$IFACE" down >/dev/null 2>&1 ) ; then
 	echo "$IFACE downed."
 fi
-
-#iw dev $IFACE del
